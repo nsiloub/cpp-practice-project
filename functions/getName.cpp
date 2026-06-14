c403e2e0916f492ca17a6451b121a3ba408155bb
NSILOU BALANDAMIO PERSEVERANCE <nsiloup@gmail.com>
2026-06-14 18:38:58 +0100

    CLEARING, IGNORE THIS

--- functions/getName.cpp
+++
@@ -1,8 +0,0 @@
-#include "getName.h"
-
-std::string getName()
-{
-    std::string name {};
-    std::getline(std::cin >> std::ws, name);
-    return name;
-}
--- headers/getAge.h
+++
@@ -1,9 +0,0 @@
-#ifndef GET_AGE
-#define GET_AGE
-
-#include <iostream>
-
-// function for getting the user age
-int getAge();
-
-#endif // GET_AGE

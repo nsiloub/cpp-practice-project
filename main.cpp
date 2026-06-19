7c8baa6bc858279e4ec0ada42021334cf1168d05
NSILOU BALANDAMIO PERSEVERANCE <nsiloup@gmail.com>
2026-06-19 13:41:53 +0100

    Question #1 from 7.x

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter a positive number: ";
+    int num {};
+    std::cin >> num;
+
+    if(num < 0)
+    {
+        std::cout << "Negative number entered.   Making positive.\n";
+        num = -num;
+        std::cout << num <<'\n';
+    }
+    else
+    {
+        std::cout << "You entered: " << num;
+    }
+
+    return  0;
+}

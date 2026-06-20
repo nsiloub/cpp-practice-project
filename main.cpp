18ffec22ba82cf77e1c25aaecf1c613f5ec00cf6
NSILOU BALANDAMIO PERSEVERANCE <nsiloup@gmail.com>
2026-06-20 12:29:16 +0100

    Question #3 - Lesson 7.x

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+int accumulate(int x) // Bad function: Impure (inconsistent outputs), but this is what they want
+{
+    static int lastValue { 0 }; // non-const variable: Bad but, this is what they want
+    lastValue += x;
+    return lastValue;
+}
+
+int main()
+{
+    std::cout << accumulate(20) << '\n';
+    std::cout << accumulate(18) << '\n';
+    std::cout << accumulate(16) << '\n';
+    std::cout << accumulate(14) << '\n';
+    std::cout << accumulate(12) << '\n';
+    std::cout << accumulate(10) << '\n';
+    std::cout << accumulate(8) << '\n';
+    std::cout << accumulate(6) << '\n';
+    std::cout << accumulate(4) << '\n';
+    std::cout << accumulate(2) << '\n';
+
+    return 0;
+}

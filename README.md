d2fcee89659787e336554939878b142773bf1d10
NSILOU BALANDAMIO PERSEVERANCE <nsiloup@gmail.com>
2026-06-23 12:17:15 +0100

    Question #1 - Lesson 8.8

---
+++ README.md
@@ -0,0 +1,3 @@
+# Question #2
+
+Write a program that prints out the letters a through z along with their ASCII codes. Use a loop variable of type ```char```.
--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+int main()
+{
+    // a = 65; z = xx; (they resolve to int8_t numbers
+    char a { 'a' };
+    // loop from a through z
+    while(a <= 'z')
+    {
+        std::cout << static_cast<char>(a) << ' ';
+        ++a;
+    }
+
+    return 0;
+}
